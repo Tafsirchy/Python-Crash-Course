5c5f69b0067f5dc83b712964598179a7fe7624c8
Tafsirchy <tafsirchy1000@gmail.com>
2026-07-10 23:17:33 +0600

    Operators

---
+++ Chapter-2/Operators.py
@@ -0,0 +1,46 @@
+# Arithmetic Operators
+
+a = 10
+
+b = 3
+
+c = a + b # Addition
+print("Addition:", c)
+
+# Asignment Operators
+a = 10 - 5 # Asign 10-5 to a
+print("a:", a)
+b =  6
+b += 3 # b = b + 3 , increment b by 3
+print("b:", b)
+
+c -= 2 # c = c - 2 , decrement c by 2
+print("c:", c)
+
+# Comparison Operators
+x = 5
+y = 10
+z = x < y # True
+print("Is x less than y?", z)
+
+z = x > y # False
+print("Is x greater than y?", z)
+
+z = 5 != 5 # False
+print("Is 5 not equal to 5?", z)
+
+z = 5 == 5 # True
+print("Is 5 equal to 5?", z)
+
+
+# Logical Operators
+a = True
+b = False
+c = a and b # False
+print("a and b:", c)
+
+c = a or b # True
+print("a or b:", c)
+
+c = not a # False
+print("not a:", c)
