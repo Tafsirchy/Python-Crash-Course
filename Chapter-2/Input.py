09a8abf5f13221c9f37bb86c750262c2c21b1997
Tafsirchy <tafsirchy1000@gmail.com>
2026-07-10 23:50:02 +0600

    Input & Type Conversion

---
+++ Chapter-2/Input.py
@@ -0,0 +1,13 @@
+a = input ("Enter a number: ")
+
+b = input ("Enter another number: ")
+
+print ("The sum of the two numbers is: ", a + b) #string concatenation instead of addition
+
+print ("The sum of the two numbers is: ", int(a) + int(b)) #convert to integer before addition
+
+
+x =  input ("Enter Your First Name: ")
+y = input ("Enter Your Last Name: ")
+
+print("Your full name is:", x + " " + y) # string concatenation to display full name
