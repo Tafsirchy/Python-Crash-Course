c0c7c4bcc50e60292f285a79be95d8c837616ec6
Tafsirchy <tafsirchy1000@gmail.com>
2026-07-10 23:25:39 +0600

    Type

---
+++ Chapter-2/Type.py
@@ -0,0 +1,20 @@
+a = 31
+type(a)
+print(type(a))
+
+
+a = "31"
+type(a)
+print(type(a))
+
+b = 3.14
+type(b)
+print(type(b))
+
+c = "Tafsir"
+type(c)
+print(type(c))
+
+str(31) # integer to string conversion
+int("32") # string to integer conversion
+float(32) # integer to float conversion
