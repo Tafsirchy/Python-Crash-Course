21427c5520152837ebd989e640943a2efe95b9bf
Tafsirchy <tafsirchy1000@gmail.com>
2026-07-11 12:48:50 +0600

    print formating

---
+++ Chapter-3 PS/Problem1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+
+print("Good Morning", name) # print the name entered by the user
+print(f"Good Afternoon {name}") # print the name entered by the user using f-string
+print("Good Evening {}".format(name)) # print the name entered by the user using format method
+print("Good Night %s" % name) # print the name entered by the user using % formatting
