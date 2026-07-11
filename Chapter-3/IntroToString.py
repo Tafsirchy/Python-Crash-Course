39f378bf98978987bb071857ec900961749a2968
Tafsirchy <tafsirchy1000@gmail.com>
2026-07-11 11:57:21 +0600

    String

--- Chapter-3/IntroToString.py
+++ Chapter-3/IntroToString.py
@@ -2,5 +2,12 @@ name  = "Tafsir"
 
 nameShort = name[0:6] # start from index 0 and go up to index 6
 print(nameShort)
+character1 = name[0] # get the first character
+print(character1)
 
 
+print(name[-5: -1]) # get characters from index -5 to -1
+print(name[1:5]) # get characters from index 1 to 6
+
+print(name[:6]) # get characters from index 0 to 6
+print(name[1:]) # get characters from index 1 to the end of the string
