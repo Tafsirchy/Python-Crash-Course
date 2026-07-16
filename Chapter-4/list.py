d0fa39498cdefd9bef9d5d4759354fd3386678f7
Tafsirchy <tafsirchy1000@gmail.com>
2026-07-16 21:26:58 +0600

    List

---
+++ Chapter-4/list.py
@@ -0,0 +1,8 @@
+friends = ["Apple", "Banana", 5, False, "Cherry", 7.5, "Date"]
+
+print(friends[0])  # Output: Apple
+friends[0] = "Apricot"  # Modifying the first element
+
+print(friends[0])  # Output: Apricot
+print(friends[-1])  # Output: Date
+print(friends[1:4])  # Output: ['Banana', 5, False]
